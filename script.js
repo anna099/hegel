9d9e1e699fbf0ee48376fc3a8699e6641863c027
anna099 <annahart099@gmail.com>
2020-06-24 14:01:18 +0100

    small caps at the beginning of sections + style upgrade

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function() {
-    resume();
+    resume(); // restore the previous light/dark mode
+    smallCaps(); // sections begin with a few words in small caps
+
     const ls = document.getElementById('lightswitch');
     ls.onclick = darkToggle;
 }
@@ -19,5 +21,20 @@ function darkToggle() {
     } else {
         document.body.classList.add('dark')
         localStorage.setItem('darkmode', true);
+    }
+}
+
+let v = null;
+function smallCaps() {
+    const ps = document.querySelectorAll('h3 + p, h4 + p');
+    for (var i = 0; i < ps.length; i++) {
+        let p = ps[i];
+        // store first 4 words, and remove them
+        let four = p.innerHTML.split(' ').slice(0, 4).join(' ');
+        p.innerHTML = p.innerHTML.replace(four, '');
+        // reinsert the first 4 words in small-caps
+        let sc = '<span style="text-transform:lowercase;font-variant:small-caps">';
+        sc += four + '</span>';
+        p.insertAdjacentHTML('afterbegin', sc);
     }
 }
