697d6ab9258261ec95baa1ff5fc64ba2bfa29940
Anna Hart <annahart099@gmail.com>
2020-12-07 11:41:34 +0000

    remove pdf building system. i dont like it

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-chapters=('introduction' 'consciousness' 'self-consciousness')
+chapters=('introduction' 'consciousness' 'self-consciousness'
+        \ 'reason-intro')
 pdfs=( "${chapters[@]/%/.pdf}" )
 FN=x-footnotes.md
 TEMP=temp.html
@@ -21,12 +22,3 @@ for i in "${chapters[@]}"; do
         rm $TEMP
     fi
 done
-
-# PDF
-for i in "${chapters[@]}"; do
-    pandoc --quiet -t html --css pdf.css -V papersize:a5 \
-        $i/[1-99]*.md $i/$FN -o pdf/$i.pdf
-done
-cd pdf
-pdfunite ${pdfs[@]} hegel.pdf
-cd ..
